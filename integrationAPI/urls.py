ebeb89259d4b5b04f7b20bd3ef9c7badc0d6427c
anarvaez1 <anarvaez@asesoftware.com>
2025-07-17 12:13:35 -0500

    Agrega endpoint root y actualiza .gitignore

--- integrationAPI/urls.py
+++ integrationAPI/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from App.interfaces.api.rootController import root_view
 
 urlpatterns = [
+    path('', root_view, name='root'),
     path('admin/', admin.site.urls),
     path('api/', include('App.interfaces.api.urls')),
+    
 ]
